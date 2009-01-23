2cbe564ece166117b3b6f794f02c7f2a5eb376f8
Integrum User <jade.meskill@integrumtech.com>
2009-01-22 20:10:18 -0700

    Compacted the evil

--- twitter-props.rb
+++ twitter-props.rb
@@ -11,12 +11,11 @@ use_template_engine :haml
 Merb::Config.use { |c|
   c[:framework]         = { :public => [Merb.root / "public", nil] }
   c[:session_store]     = 'none'
-  c[:exception_details] = true
-  c[:log_level]         = :debug # or error, warn, info or fatal
-  c[:log_stream]        = STDOUT
+  c[:exception_details] = false
+  c[:log_level]         = :error # or error, warn, info or fatal
   c[:log_file]          = Merb.root / "log" / "merb.log"
-  c[:reload_classes]    = true
-  c[:reload_templates]  = true
+  c[:reload_classes]    = false
+  c[:reload_templates]  = false
 }
 
 COUCHDB = "http://10.0.40.99:5984/twitterprops"
@@ -42,20 +41,12 @@ class Users < Merb::Controller
       rows = CouchRest::Model.default_database.view("messages/by_#{term}")["rows"].first['value'].collect {|n| TempSort.new(n)}
       self.instance_variable_set("@#{term}", rows.sort {|a,b| b.send(term) <=> a.send(term) } )
     end
-    
     render
   end
-  
+
   def show
-    @messages = CouchRest::Model.default_database.view("messages/by_handle", :key => params[:user] )["rows"].collect {|n| TempSort.new(n['value']) }
-    @messages.sort! {|a,b| b.published_at <=> a.published_at }
-    
-    begin
-      @stats = TempSort.new(CouchRest::Model.default_database.view("messages/by_receiver_totals", :key => params[:user] )["rows"].first['value'])
-    rescue
-      @stats = TempSort.new({:totals => 0, :props => 0, :drops => 0})
-    end
-    
+    @messages = CouchRest::Model.default_database.view("messages/by_handle", :key => params[:user] )["rows"].collect {|n| TempSort.new(n['value']) }.sort {|a,b| b.published_at <=> a.published_at }
+    @stats = TempSort.new((CouchRest::Model.default_database.view("messages/by_receiver_totals", :key => params[:user] )["rows"].first || {'value' => {'totals' => 0, 'props' => 0, 'drops' => 0}})['value'])
     render
   end
 end
@@ -71,6 +62,5 @@ end
 
 class Message < CouchRest::Model
   key_accessor :author, :receiver, :content, :published_at, :status_uri, :image_uri, :tweet_id
-  
   timestamps!
 end
